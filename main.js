5921d9b3668d761aebc3cc6a531ac7354475e090
kyu099 <ryu_gelus1277@icloud.com>
2023-08-25 02:13:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 const ctx = canvas.getContext("2d");
 const share = document.getElementById("sharebutton");
+const undo = document.getElementById("undo");
 const hcabinet = document.getElementById("hcabinet");
 const vcabinet = document.getElementById("vcabinet");
 const waitings = document.getElementById("waitings");
 
-let players = [1, 1, 1, 1];
 let waiting = [];
 let cabinets = [];
+let order = [];
 
 function countPlayers() {
-    let x = 0;
-    for (let i = 0; i < players.length; i++){
-        x += players[i];
+    let playercount = 0;
+    for(let i = 0; i < cabinets.length; i++) {
+        if(cabinets[i].playing == 1){
+            playercount++;
+        }
     }
-    return x;
+    return playercount;
 }
 
 //バツ印を描画する関数
@@ -66,15 +69,9 @@ function draw(ctx) {
         ctx.fill();
     }
 
-    let playercount = 0;
-    for(let i = 0; i < cabinets.length; i++) {
-        if(cabinets[i].playing == 1){
-            playercount++;
-        }
-    }
     ctx.fillStyle = "red"
     ctx.font = '72px sans-serif';
-    ctx.fillText("空き:" + String(cabinets.length - playercount), 40, 1320);
+    ctx.fillText("空き:" + String(cabinets.length - countPlayers()), 40, 1320);
     ctx.fillText("待ち:" + String(waiting.length), 40, 1400);
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
 }
@@ -85,7 +82,7 @@ canvas.addEventListener("click", (e) => {
     let x = e.clientX - canvas.getBoundingClientRect().left;
     let y = e.clientY - canvas.getBoundingClientRect().top;
     for(let i = 0; i < cabinets.length; i++) {
-        if((x-cabinets[i].x)**2 + (y-cabinets[i].y)**2 < 120**2){
+        if((x-cabinets[i].x)**2 + (y-cabinets[i].y)**2 < 100**2){
             cabinets[i].playing = cabinets[i].playing * (-1);
         }
     }
@@ -93,50 +90,60 @@ canvas.addEventListener("click", (e) => {
     if(waitings.checked){
         let n = 1;
         for(i = 0; i < cabinets.length; i++) {
-            if((x-cabinets[i].x)**2 + (y-cabinets[i].y)**2 < (60 + 120)**2){
+            if((x-cabinets[i].x)**2 + (y-cabinets[i].y)**2 < 100**2){
                 n = n * 0;
             }
         }
         if(n > 0) {
             waiting.push({x: x, y: y});
+            order.push("waiting");
         }
     } else if(vcabinet.checked){
         let n = 1;
         for(i = 0; i < cabinets.length; i++) {
-            if((x-cabinets[i].x)**2 + (y-cabinets[i].y)**2 < (120 + 120)**2){
+            if((x-cabinets[i].x)**2 + (y-cabinets[i].y)**2 < (100 + 100)**2){
                 n = n * 0;
             }
         }
         if(n > 0) {
             cabinets.push({x: x, y: y, direction: "v", playing: -1});
+            order.push("cabinet");
         }
     } else if(hcabinet.checked){
         let n = 1;
         for(i = 0; i < cabinets.length; i++) {
-            if((x-cabinets[i].x)**2 + (y-cabinets[i].y)**2 < (120 + 120)**2){
+            if((x-cabinets[i].x)**2 + (y-cabinets[i].y)**2 < (100 + 100)**2){
                 n = n * 0;
             }
         }
         if(n > 0) {
             cabinets.push({x: x, y: y, direction: "h", playing: -1});
+            order.push("cabinet");
         }
     }
     draw(ctx);
     console.log(cabinets);
 }, false);
 
+undo.onclick = () => {
+    if(order.length > 0){
+        let last = order.pop();
+        if(last == "waiting"){
+            waiting.pop();
+        } else if(last == "cabinet"){
+            cabinets.pop();
+        }
+    }
+    draw(ctx);
+    console.log("undo : ", undo);
+}
+
 share.onclick = () => {
     let text ="";
     let now = new Date();
-    let playercount = 0;
-    for(let i = 0; i < cabinets.length; i++) {
-        if(cabinets[i].playing == 1){
-            playercount++;
-        }
-    }
     text = `#嘘モナ恋情報
 ${now.getHours()}時${now.getMinutes()}分
-チュウニズム 空き${String(cabinets.length - playercount)} 待ち${waiting.length}
+チュウニズム 空き${String(cabinets.length - countPlayers())} 待ち${waiting.length}
 嘘モナ恋情報共有はこちらから！
 ↓ ↓ ↓
 https://kyu099.github.io/usomonacoi/`
